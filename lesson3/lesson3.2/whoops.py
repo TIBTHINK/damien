adf99675da8727c354e680ddad8b84c8d9cfa27b
tibthink <bent23jam@gmail.com>
2021-06-27 00:27:52 -0400

    lesson 3.2 out

--- lesson3/lesson3.2/whoops.py
+++ lesson3/lesson3.2/whoops.py
@@ -1,12 +1,6 @@
-# Try and try again i will
-using the try function is good for \
-hidding error codes with your own
-## Hidding the fucky wuckys
-an example of a program using the try function looks like this
-```python
 try:                        # Using try for the main part of the code
     print("are you ready?") # simple example program
     input()
 except KeyboardInterrupt:   # if pressed ctrl + c while still in the try loop the script will exit
     exit()
-```
+    
