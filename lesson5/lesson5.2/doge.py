9ba86c021872ba2a04d98065662cbd7701e4b285
tibthink <bent23jam@gmail.com>
2021-08-30 15:57:43 +0000

    5.2 is done

--- lesson5/lesson5.2/doge.py
+++ lesson5/lesson5.2/doge.py
@@ -1,20 +1,21 @@
+# Importing required libraries
 from time import sleep
 import requests
 import json
 import sys
 
-try:
-    while True:
-        response = requests.get("https://api.binance.us/api/v3/ticker/price?symbol=DOGEUSD")
-        output = response.json()
-        data = json.dumps(output['price'])
-        punctuation = '''"'''
-        remove_punct = ""
-        for character in data:
-            if character not in punctuation:
-                remove_punct = remove_punct + character
-        price = remove_punct
-        print('current price of doge: '+ price, end='\r')
+try:        # Havinging the code run in a try function so if we cancle it we dont get errors
+    while True:     # A forever loop to get current price 
+        response = requests.get("https://api.binance.us/api/v3/ticker/price?symbol=DOGEUSD")    # Requesting binance api for current price
+        output = response.json()    # Saving the request as a json varibal
+        data = json.dumps(output['price'])  # Parsing the repsones for current price
+        punctuation = '''"'''                           # this
+        remove_punct = ""                               # is
+        for character in data:                          # to 
+            if character not in punctuation:            # make
+                remove_punct = remove_punct + character # it
+        price = remove_punct                            # fancy
+        print('current price of doge: '+ price, end='\r') #printing the price from the api and updating the line instead of making a new one every loop
 
 
 except KeyboardInterrupt:
--- lesson5/lesson5.2/lesson5.2.md
+++ lesson5/lesson5.2/lesson5.2.md
@@ -4,3 +4,27 @@ Its always good to get information from the outside world
 
 in this lesson we are going to get working on a script that \
 fetches a api to find the current price of doge coin
+```python
+# Importing required libraries
+from time import sleep
+import requests
+import json
+import sys
+
+try:        # Havinging the code run in a try function so if we cancle it we dont get errors
+    while True:     # A forever loop to get current price 
+        response = requests.get("https://api.binance.us/api/v3/ticker/price?symbol=DOGEUSD")    # Requesting binance api for current price
+        output = response.json()    # Saving the request as a json varibal
+        data = json.dumps(output['price'])  # Parsing the repsones for current price
+        punctuation = '''"'''                           # this
+        remove_punct = ""                               # is
+        for character in data:                          # to 
+            if character not in punctuation:            # make
+                remove_punct = remove_punct + character # it
+        price = remove_punct                            # fancy
+        print('current price of doge: '+ price, end='\r') #printing the price from the api and updating the line instead of making a new one every loop
+
+
+except KeyboardInterrupt:
+    exit()
+```
